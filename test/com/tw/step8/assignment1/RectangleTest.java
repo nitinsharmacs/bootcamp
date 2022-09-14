61f90ec08e8665188073d9bb998e7600ed71ab5e
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-09-14 13:10:55 +0530

    Implement factory methods for creating rectangle and square in Rectangle

--- test/com/tw/step8/assignment1/RectangleTest.java
+++ test/com/tw/step8/assignment1/RectangleTest.java
@@ -7,24 +7,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class RectangleTest {
   @Test
   void shouldDetermineRectangleArea() {
-    Rectangle rectangle = new Rectangle(1, 1);
+    Rectangle rectangle = Rectangle.createRectangle(1, 1);
+
     double area = rectangle.area();
 
     assertEquals(1, area);
-
-    Rectangle rectangle2 = new Rectangle(0.5, 2);
-
-    assertEquals(1.0, rectangle2.area());
   }
 
   @Test
-  void shouldDeterminePerimeterOfRectangle() {
-    Rectangle rectangle = new Rectangle(1, 1);
+  void shouldDetermineRectanglePerimeter() {
+    Rectangle rectangle = Rectangle.createRectangle(1, 1);
 
-    assertEquals(4, rectangle.perimeter());
+    double perimeter = rectangle.perimeter();
 
-    Rectangle rectangle2 = new Rectangle(1.5, 0.5);
+    assertEquals(4, perimeter);
+  }
 
-    assertEquals(4.0, rectangle2.perimeter());
+  @Test
+  void shouldDetermineSquareArea() {
+    Rectangle square = Rectangle.createSquare(2);
+
+    double area = square.area();
+
+    assertEquals(4, area);
+  }
+
+  @Test
+  void shouldDetermineSquarePerimeter() {
+    Rectangle square = Rectangle.createSquare(2);
+
+    double perimeter = square.perimeter();
+    
+    assertEquals(8, perimeter);
   }
 }
