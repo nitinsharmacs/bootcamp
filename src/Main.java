2b11c0d78150452baf66809a2e955a8c3ce79d01
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-09-14 11:23:25 +0530

    Integerated Rectangle with Project Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import com.tw.step8.assignment1.Rectangle;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Rectangle rectangle = new Rectangle(2, 3);
+    System.out.printf("Area %f\n", rectangle.area());
+    System.out.printf("Perimeter %f\n", rectangle.perimeter());
   }
 }
