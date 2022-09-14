25904a258961d9ded7f5d1815a9ea2bd11fd9f91
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2022-09-14 11:45:29 +0530

    Removed Main

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import com.tw.step8.assignment1.Rectangle;
-
-public class Main {
-  public static void main(String[] args) {
-    Rectangle rectangle = new Rectangle(2, 3);
-    System.out.printf("Area %f\n", rectangle.area());
-    System.out.printf("Perimeter %f\n", rectangle.perimeter());
-  }
-}
